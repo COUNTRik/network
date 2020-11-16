7cb36fea038165cbb9aec55dd5ca42669643a70e
Albert <countrik@yandex.ru>
2020-11-16 17:24:57 +0300

    update

--- scripts/office1Router.sh
+++ scripts/office1Router.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Включаем прохождение пакетов между сетевыми интерфейсами
-# sysctl net.ipv4.conf.all.forwarding=1
+sysctl net.ipv4.conf.all.forwarding=1
 
 # Удаляем маргрут по умолчанию и ставим нужные нам маршруты
 ip route del default via 10.0.2.2 dev eth0
